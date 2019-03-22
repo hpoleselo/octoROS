0e21f2887515838b0b43c95ab7e1feab067a40cf
Henrique Poleselo <hpoleselo@gmail.com>
2019-03-22 12:48:55 -0300

    Changes made to retrieve more info from the printer

--- src/messenger.py
+++ src/messenger.py
@@ -12,7 +12,7 @@ octoIP = "http://127.0.0.1"
 octoPort = ":5000"
 apiKey = "A5B916E2F8724A239572AEB63CA3D682"
 
-
+# json key with the API Key
 standardHeader = {'X-Api-Key': apiKey}
 
 # Connection handling
@@ -28,7 +28,10 @@ def modelSelection():
 
 
 def printModel(modelName):
-    # the modelname with the .gcode
+    """" the modelname with the .gcode
+    in: modelname
+    return:  
+    """
     printData = {'command': 'select', 'print': True}
     url = _url('files/local/{}'.format(modelName))
     return requests.post(url, json=printData, headers=standardHeader, timeout=5)
@@ -39,7 +42,12 @@ def progressTracking():
     response = requests.get(_url('job'), headers=standardHeader, timeout=5)
     return (response.json()['progress']['completion'])
 
+# Realize that all the functions are the same the only thing that changes is the jsonData, which is basically
+# the command we're sending!
+
 def cancelPrinting():
+    # Format is: command: YOUR_COMMAND
+    # And we are passing it as a JSON (with key values)
     jsonData = {'command':'cancel'}
     return requests.post(_url('job'), headers=standardHeader, timeout=5, json=jsonData)
 
@@ -57,13 +65,111 @@ def getprinterInfo():
     tool0Temp = response.json()['temperature']['tool0']['actual']
     tool1Temp = response.json()['temperature']['tool1']['actual']
     bedTemp = response.json()['temperature']['bed']['actual']
-    # 
-    #
-    #
-    #
-    return tool0Temp, tool1Temp, bedTemp
+    
+    # TEST / state details -> retorna booleano
+    isPrinting = response.json()['state']['flag']['printing']
+    isPaused = response.json()['state']['flag']['pausing']
+    
+    # TEST / file details
+    fileName = response.json()['files']['name']
+    # in minutes
+    estimatedTime = response.json()['files']['gcodeAnalysis']['estimatedPrintTime']
+    
+    return tool0Temp, tool1Temp, bedTemp, isPrinting, isPaused, fileName, estimatedTime
 
+def getTimelapse():
+    gtl_response = requests.get(_url('timelapse'), headers=standardHeader, timeout=5)
+    return gtl_response
 
 def _url(path):
+    """ Function to pass the URL """
     octoAddress = octoIP + octoPort + '/api/'
     return octoAddress + path
+#!/usr/bin/env python2.7
+
+""" octoROS - integrating octoprint with ROS, so it's possible to control your 3d printer from your robot
+This library is based in the octoprint api, more info can be found here:
+http://docs.octoprint.org/en/master/api/index.html
+"""
+
+
+import requests
+
+octoIP = "http://127.0.0.1"
+octoPort = ":5000"
+apiKey = "A5B916E2F8724A239572AEB63CA3D682"
+
+# json key with the API Key
+standardHeader = {'X-Api-Key': apiKey}
+
+# Connection handling
+def connectToPrinter():
+    connectionData = {"command": "connect", "port": "/dev/ttyACM0", "baudrate": 115200, "printerProfile": "_default",
+                      "save": True, "autoconnect": True}
+    return requests.post(_url("connection"), json=connectionData, headers=standardHeader, timeout=5)
+
+
+# File operations
+def modelSelection():
+    pass
+
+
+def printModel(modelName):
+    """" the modelname with the .gcode
+    in: modelname
+    return:  
+    """
+    printData = {'command': 'select', 'print': True}
+    url = _url('files/local/{}'.format(modelName))
+    return requests.post(url, json=printData, headers=standardHeader, timeout=5)
+
+
+# Job operations
+def progressTracking():
+    response = requests.get(_url('job'), headers=standardHeader, timeout=5)
+    return (response.json()['progress']['completion'])
+
+# Realize that all the functions are the same the only thing that changes is the jsonData, which is basically
+# the command we're sending!
+
+def cancelPrinting():
+    # Format is: command: YOUR_COMMAND
+    # And we are passing it as a JSON (with key values)
+    jsonData = {'command':'cancel'}
+    return requests.post(_url('job'), headers=standardHeader, timeout=5, json=jsonData)
+
+def pausePrinting():
+    jsonData = {'command':'pause', 'action':'pause'}
+    return requests.post(_url('job'), headers=standardHeader, timeout=5, json=jsonData)
+
+def resumePrinting():
+    jsonData = {'command':'pause', 'action':'resume'}
+    return requests.post(_url('job'), headers=standardHeader, timeout=5, json=jsonData)
+
+# Printer operations
+def getprinterInfo():
+    response = requests.get(_url('printer'), headers=standardHeader, timeout=5)
+    tool0Temp = response.json()['temperature']['tool0']['actual']
+    tool1Temp = response.json()['temperature']['tool1']['actual']
+    bedTemp = response.json()['temperature']['bed']['actual']
+    
+    # TEST / state details -> retorna booleano
+    isPrinting = response.json()['state']['flag']['printing']
+    isPaused = response.json()['state']['flag']['pausing']
+    
+    # TEST / file details
+    fileName = response.json()['files']['name']
+    # in minutes
+    estimatedTime = response.json()['files']['gcodeAnalysis']['estimatedPrintTime']
+    
+    return tool0Temp, tool1Temp, bedTemp, isPrinting, isPaused, fileName, estimatedTime
+
+def getTimelapse():
+    gtl_response = requests.get(_url('timelapse'), headers=standardHeader, timeout=5)
+    return gtl_response
+
+def _url(path):
+    """ Function to pass the URL """
+    octoAddress = octoIP + octoPort + '/api/'
+    return octoAddress + path
+
--- src/octoROS.py
+++ src/octoROS.py
@@ -13,7 +13,7 @@ from std_msgs.msg import String
 
 import messenger
 
-file_to_print = 'FlexiRexColor1.gcode'
+fileToPrint = 'FlexiRexColor1.gcode'
 
 class RosInterface(object):
     def __init__(self):
@@ -23,12 +23,12 @@ class RosInterface(object):
         print("initialized")
 
     def performConnection(self):
-        self.print_pub.publish("Trying to connect to 3dprinter")
+        self.print_pub.publish("Trying to connect to the 3D Printer...")
         connection = messenger.connectToPrinter()
         if connection.status_code != 204:
             # TODO change all exceptions to the right ones
             raise Exception('Could not connect to printer, error code: {}'.format(connection.status_code))
-        self.print_pub.publish("Connection Succeed")
+        self.print_pub.publish("Connection succeeded")
 
     def printAndGetStatus(self, modelName):
         printing = messenger.printModel(modelName)
@@ -42,7 +42,11 @@ class RosInterface(object):
             print("Progress: {}%".format(progress))
             # TODO change to a action
             self.print_pub.publish("Progress: {}%".format(progress))
-            tool0Temp, tool1Temp, bedTemp = messenger.getprinterInfo()
+            tool0Temp, tool1Temp, bedTemp, isPrinting, isPaused, fileName, estimatedTime = messenger.getprinterInfo()
+
+            # TEST
+            print 'file name:', fileName
+            print 'estimated time left: ', estimatedTime
             # TODO Need to see the type returned by getPrinterInfo, so I can make a msg and send it to the right topic
             print("Tool 0 temp: {}C, tool 1 temp: {}C, bed temp: {}C".format(tool0Temp, tool1Temp, bedTemp))
             self.print_pub.publish(
@@ -57,7 +61,7 @@ def main(args):
     # ROS was not catching interrupt exceptions, so I had to disable signals and use the KeyboardInterrupt exception
     rospy.init_node('printerWatcher', anonymous=True, disable_signals=True)
     interf = RosInterface()
-    interf.printAndGetStatus(file_to_print)
+    interf.printAndGetStatus(fileToPrint)
 
 
 if __name__ == '__main__':
@@ -66,3 +70,76 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Shutting down")
         messenger.cancelPrinting()
+#!/usr/bin/env python
+
+""" octoROS - integrating octoprint with ROS, so it's possible to control your 3d printer from your robot
+This library is based in the octoprint api, more info can be found here:
+http://docs.octoprint.org/en/master/api/index.html
+"""
+
+import sys
+
+import rospy
+from std_msgs.msg import Bool
+from std_msgs.msg import String
+
+import messenger
+
+fileToPrint = 'FlexiRexColor1.gcode'
+
+class RosInterface(object):
+    def __init__(self):
+        self.print_pub = rospy.Publisher('printer3d', String, queue_size=100)
+        self.printFinished_pub = rospy.Publisher('printer3d/finishedPrinting', Bool, queue_size=10)
+        self.rate = rospy.Rate(0.1)  # 0.1 hz
+        print("initialized")
+
+    def performConnection(self):
+        self.print_pub.publish("Trying to connect to the 3D Printer...")
+        connection = messenger.connectToPrinter()
+        if connection.status_code != 204:
+            # TODO change all exceptions to the right ones
+            raise Exception('Could not connect to printer, error code: {}'.format(connection.status_code))
+        self.print_pub.publish("Connection succeeded")
+
+    def printAndGetStatus(self, modelName):
+        printing = messenger.printModel(modelName)
+        if printing.status_code != 204:
+            pass
+            # raise Exception('Could not print, status code: {}'.format(printing.status_code))
+        self.print_pub.publish("Starting to print model {}".format(modelName))
+        progress = messenger.progressTracking()
+        while progress < 100 and not rospy.is_shutdown():
+            progress = messenger.progressTracking()
+            print("Progress: {}%".format(progress))
+            # TODO change to a action
+            self.print_pub.publish("Progress: {}%".format(progress))
+            tool0Temp, tool1Temp, bedTemp, isPrinting, isPaused, fileName, estimatedTime = messenger.getprinterInfo()
+
+            # TEST
+            print 'file name:', fileName
+            print 'estimated time left: ', estimatedTime
+            # TODO Need to see the type returned by getPrinterInfo, so I can make a msg and send it to the right topic
+            print("Tool 0 temp: {}C, tool 1 temp: {}C, bed temp: {}C".format(tool0Temp, tool1Temp, bedTemp))
+            self.print_pub.publish(
+                "Tool 0 temp: {}C, tool 1 temp: {}C, bed temp: {}C".format(tool0Temp, tool1Temp, bedTemp))
+            self.rate.sleep()
+        print("Successful print!")
+        self.print_pub.publish("Successful print")
+        self.printFinished_pub.publish(True)
+
+
+def main(args):
+    # ROS was not catching interrupt exceptions, so I had to disable signals and use the KeyboardInterrupt exception
+    rospy.init_node('printerWatcher', anonymous=True, disable_signals=True)
+    interf = RosInterface()
+    interf.printAndGetStatus(fileToPrint)
+
+
+if __name__ == '__main__':
+    try:
+        main(sys.argv)
+    except KeyboardInterrupt:
+        print("Shutting down")
+        messenger.cancelPrinting()
+
